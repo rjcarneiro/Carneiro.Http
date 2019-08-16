5add9b279cbb82ab4b1c5ddc31e40423663241ff
Ricardo Carneiro <rjorge.carneiro@gmail.com>
2019-08-16 09:45:53 -0500

    Add comments

--- src/Carneiro.Http/HttpOrchestrator.cs
+++ src/Carneiro.Http/HttpOrchestrator.cs
@@ -16,7 +16,6 @@ namespace Carneiro.Http
         /// <summary>
         /// Initializes a new instance of the <see cref="HttpOrchestrator"/> class.
         /// </summary>
-        /// <param name="options">The options.</param>
         /// <param name="httpClientFactory">The HTTP client factory.</param>
         /// <exception cref="ArgumentNullException">options</exception>
         /// <exception cref="InvalidOperationException">Missing a valid url for the client endpoint.</exception>
@@ -26,12 +25,18 @@ namespace Carneiro.Http
         }
 
         /// <summary>
-        /// Does an Http Get asynchronously.
+        /// Does an Http Get request asynchronously.
         /// </summary>
         /// <param name="uri">The handler uri for the http request.</param>
         /// <returns>An <see cref="HttpResponseMessage"/> with the response.</returns>
         public Task<HttpResponseMessage> GetAsync(string uri) => _httpClient.GetAsync(uri);
 
+        /// <summary>
+        /// Does an Http Get request asynchronously.
+        /// </summary>
+        /// <typeparam name="T">The input type.</typeparam>
+        /// <param name="uri">The handler uri for the http request.</param>
+        /// <returns>The type defined in <typeparamref name="T"/>.</returns>
         public async Task<T> GetAsync<T>(string uri)
         {
             HttpResponseMessage response = await GetAsync(uri);
@@ -40,21 +45,21 @@ namespace Carneiro.Http
         }
 
         /// <summary>
-        /// Does an Http Post asynchronously.
+        /// Does an Http Post request asynchronously.
         /// </summary>
         /// <typeparam name="T">The input type.</typeparam>
         /// <param name="uri">The handler uri for the http request.</param>
-        /// <param name="model">The body to go on the body.</param>
+        /// <param name="model">The model that goes on the body.</param>
         /// <returns>An <see cref="HttpResponseMessage"/> with the response.</returns>
         public Task<HttpResponseMessage> PostAsync<T>(string uri, T model) => _httpClient.PostAsync(uri, ToStringContent<T>(model));
 
         /// <summary>
-        /// 
+        /// Does an Http Post request asynchronously.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="uri"></param>
+        /// <typeparam name="T">The input type.</typeparam>
+        /// <param name="uri">The handler uri for the http request.</param>
         /// <param name="model"></param>
-        /// <returns></returns>
+        /// <returns>The type defined in <typeparamref name="T"/>.</returns>
         public async Task<T> PostAsync<T>(string uri, object model) where T : class
         {
             HttpResponseMessage response = await _httpClient.PostAsync(uri, ToStringContent(model));
@@ -63,21 +68,21 @@ namespace Carneiro.Http
         }
 
         /// <summary>
-        /// 
+        /// Does an Http Put request asynchronously.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="uri"></param>
+        /// <typeparam name="T">The input type.</typeparam>
+        /// <param name="uri">The handler uri for the http request.</param>
         /// <param name="model"></param>
-        /// <returns></returns>
+        /// <returns>An <see cref="HttpResponseMessage"/> with the response.</returns>
         public Task<HttpResponseMessage> PutAsync<T>(string uri, T model) => _httpClient.PutAsync(uri, ToStringContent(model));
 
         /// <summary>
-        /// 
+        /// Does an Http Put request asynchronously.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="uri"></param>
+        /// <typeparam name="T">The input type.</typeparam>
+        /// <param name="uri">The handler uri for the http request.</param>
         /// <param name="model"></param>
-        /// <returns></returns>
+        /// <returns>The type defined in <typeparamref name="T"/>.</returns>
         public async Task<T> PutAsync<T>(string uri, object model) where T : class
         {
             HttpResponseMessage response = await _httpClient.PutAsync(uri, ToStringContent(model));
@@ -86,10 +91,10 @@ namespace Carneiro.Http
         }
 
         /// <summary>
-        /// 
+        /// Does an Http Delete request asynchronously.
         /// </summary>
-        /// <param name="uri"></param>
-        /// <returns></returns>
+        /// <param name="uri">The handler uri for the http request.</param>
+        /// <returns>An <see cref="HttpResponseMessage"/> with the response.</returns>
         public Task<HttpResponseMessage> DeleteAsync(string uri) => _httpClient.DeleteAsync(uri);
 
         private StringContent ToStringContent<T>(T model) => new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
